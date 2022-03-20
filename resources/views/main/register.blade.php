383c6e6320eb164a5d788e24d88a593a71f0a421
Erikas1452 <erikastumanovas@gmail.com>
2022-03-20 13:44:12 +0200

    Main page/ Registration page styling

---
+++ resources/views/main/register.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+      
+        <title>Serenity Bootstrap Template - Index</title>
+        <meta content="" name="description">
+        <meta content="" name="keywords">
+      
+        <!-- Favicons -->
+        <link href="assets/img/favicon.png" rel="icon">
+        <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+      
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+      
+        <!-- Vendor CSS Files -->
+        <link href="{{asset('assets/vendor/aos/aos.css')}}" rel="stylesheet">
+        <link href="{{asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+        <link href="{{asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+        <link href="{{asset('assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+        <link href="{{asset('assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+        <link href="{{asset('assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+
+        <link href="{{asset('assets/css/register.css')}}" rel="stylesheet">
+        
+        <!-- Template Main CSS File -->
+        <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+      
+        <!-- =======================================================
+        * Template Name: Serenity - v4.7.0
+        * Template URL: https://bootstrapmade.com/serenity-bootstrap-corporate-template/
+        * Author: BootstrapMade.com
+        * License: https://bootstrapmade.com/license/
+        ======================================================== -->
+      </head>
+
+<body>
+
+    @include('snipets.header')
+
+  <main id="main">
+
+    <div class="padding container d-flex justify-content-center">
+        <div class="col-md-10 col-md-offset-1">
+            <form class="signup-form">
+                <h2 class="text-center">SIGNUP NOW</h2>
+                <hr>
+                <div class="form-group"> <input type="text" class="form-control" placeholder="Full Name" required="required"> </div>
+                <div class="form-group"> <input type="email" class="form-control" placeholder="Email Address" required="required"> </div>
+                <div class="form-group"> <input type="text" class="form-control" placeholder="User Name" required="required"> </div>
+                <div class="form-group"> <input type="text" class="form-control" placeholder="Password" required="required"> </div>
+                <div class="form-group text-center"> <button type="submit" class="btn btn-blue btn-block">Start Now</button> </div>
+            </form>
+        </div>
+    </div>
+
+  </main><!-- End #main -->
+
+  @include('snipets.footer')
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="{{asset('assets/vendor/purecounter/purecounter.js')}}"></script>
+  <script src="{{asset('assets/vendor/aos/aos.js')}}"></script>
+  <script src="{{asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+  <script src="{{asset('assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <script src="{{asset('assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+  <script src="{{asset('assets/vendor/waypoints/noframework.waypoints.js')}}"></script>
+  <script src="{{asset('assets/vendor/php-email-form/validate.js')}}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{asset('assets/js/main.js')}}"></script>
+
+</body>
+
+</html>
