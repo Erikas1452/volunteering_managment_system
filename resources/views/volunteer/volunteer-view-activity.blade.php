7ebb203707c12a51e29e80ca93135366278cb3cc
Erikas1452 <erikastumanovas@gmail.com>
2022-04-26 19:52:44 +0300

    Activity view update

--- resources/views/volunteer/volunteer-view-activity.blade.php
+++ resources/views/volunteer/volunteer-view-activity.blade.php
@@ -8,7 +8,75 @@
       </div>
     </section><!-- End Cta Section -->
   
-    <div class="container-fluid global_container index_container">
+    <section id="about" class="about">
+        <div class="container">
+  
+          <div class="">
+            <div class="col-lg-11">
+              <div class="row justify-content-end">
+  
+                <div class="col-lg-12 col-md-12 col-12 d-md-flex align-items-md-stretch">
+                  <div class="count-box py-5">
+                    <h1 style="font-weight: 700;
+                    color: #3c4133;">
+                    Savanorystė: {{$data['activity'][0]->name}}</h1>
+                  </div>
+                </div>
+
+  
+              </div>
+            </div>
+          </div>
+  
+          <div class="row">
+  
+            <div class="col-lg-6 video-box align-self-baseline position-relative">
+              <img src="{{asset($data['activity'][0]->activity_photo)}}" class="img-fluid" alt="">
+            </div>
+  
+            <div class="col-lg-6 pt-3 pt-lg-0 content">
+              <h3>Sukurta organizacijos:</h3>
+              <p class="fst-italic">{{$data['organization'][0]->name}}</p>
+              <p class="fst-italic">{{$data['organization'][0]->email}}</p>
+              <p class="fst-italic">Savanorių limitas: {{$data['activity'][0]->people_limit}}</p>
+              <p class="fst-italic">laisvų vietų: {{$data['activity'][0]->people_limit - $data['activity'][0]->people_registered}}</p>
+              <h3>Savanorystės Aprašymas:</h3>
+              <p class="fst-italic">{{$data['activity'][0]->long_desc}}</p>
+
+              <div class="row">
+                <div class="col-md-6">
+                    <h3>Pradžia</h3>
+                    <h4>{{$data['activity'][0]->start_date}}<h4>
+                </div>
+                <div class="col-md-6">
+                    <h3>Pabaiga</h3>
+                    <h4>{{$data['activity'][0]->end_date}}<h4>
+                </div>
+
+            </div>
+
+            </div>
+  
+          </div>
+  
+        </div>
+      </section><!-- End About Section -->
+  
+      
+
+      <!-- ======= Work Process Section ======= -->
+      <section id="work-process" class="work-process">
+        <div class="container">
+  
+          <div class="section-title" data-aos="fade-up">
+            <a href="{{}}"><h2 style="color: #94c045">Registruotis</h2></a>
+          </div>
+  
+        </div>
+      </section><!-- End Work Process Section -->
+  
+
+    {{-- <div class="container-fluid global_container index_container">
             <div class="box">
                 <div class="row">
                     <div class="col-md-12">
@@ -21,8 +89,8 @@
 
                 <div class="row">
 
-                    <div class="col-md-4">
-                        <img width="380px" height="auto" src="{{asset($data['activity'][0]->activity_photo)}}">
+                    <div class="col-md-2">
+                        <img width="200px" height="auto" src="{{asset($data['activity'][0]->activity_photo)}}">
                     </div>
 
                     <div class="col-md-8">
@@ -59,7 +127,7 @@
                 <a class="btn btn-primary" href="{{}}">Registruotis</a>
                 <br>
             </div>
-    </div>
+    </div> --}}
     
 
   </main><!-- End #main -->
